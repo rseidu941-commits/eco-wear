1d1d92b5c7e2a899c8a32aaed2e1817611245496
Rukayatu <rseidu941@gmail.com>
2026-07-28 19:14:47 +0200

    update:product card compoent completed

---
+++ src/Components/Productcard/ProductCard.jsx
@@ -0,0 +1,36 @@
+import "./ProductCard.scss";
+import Button from "../Button/Button";
+
+function ProductCard({ product }) {
+  return (
+    <article className="product-card">
+
+      <div className="product-card__image-wrapper">
+        <img
+          src={product.image}
+          alt={product.name}
+          className="product-card__image"
+        />
+      </div>
+
+      <div className="product-card__content">
+
+        <h3 className="product-card__title">
+          {product.name}
+        </h3>
+
+        <p className="product-card__price">
+          €{product.price}
+        </p>
+
+        <div className="product-card__button">
+          <Button text="VIEW DETAILS" />
+        </div>
+
+      </div>
+
+    </article>
+  );
+}
+
+export default ProductCard;
