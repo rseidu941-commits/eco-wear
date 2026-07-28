3e548e0dceebadd8c8beabbbeb292544dd9130f6
Rukayatu <rseidu941@gmail.com>
2026-07-28 18:17:54 +0200

    images added in public

---
+++ src/data/products.js
@@ -0,0 +1,83 @@
+const products = [
+  {
+    id: 1,
+    name: "Midnight Silk Dress",
+    category: "Dresses",
+    price: 280,
+    image: "..src/..asserts/..images/18kgold-bangle.png",
+  },
+
+  {
+    id: 2,
+    name: "Black Pleated Dress",
+    category: "Dresses",
+    price: 280,
+    image: "/images/black-pleated-dress.png",
+  },
+
+  {
+    id: 3,
+    name: "Burgundy Midi Dress",
+    category: "Dresses",
+    price: 240,
+    image: "/images/burgundy-midi-dress.png",
+  },
+
+  {
+    id: 4,
+    name: "Emerald Maxi Dress",
+    category: "Dresses",
+    price: 320,
+    image: "/images/emerald-maxi-dress.png",
+  },
+
+  {
+    id: 5,
+    name: "Charcoal Grey Sheath Dress",
+    category: "Dresses",
+    price: 180,
+    image: "/images/charcoal-grey-sheath.png",
+  },
+
+  {
+    id: 6,
+    name: "18K Gold Bangle",
+    category: "Jewelry",
+    price: 190,
+    image: "/images/18k-gold-bangle.png",
+  },
+
+  {
+    id: 7,
+    name: "Gold Chain Necklace",
+    category: "Jewelry",
+    price: 350,
+    image: "/images/gold-chain-necklace.png",
+  },
+
+  {
+    id: 8,
+    name: "Diamond Earrings",
+    category: "Jewelry",
+    price: 250,
+    image: "/images/diamond-earrings.png",
+  },
+
+  {
+    id: 9,
+    name: "Classic Pearl Necklace",
+    category: "Jewelry",
+    price: 150,
+    image: "/images/classic-pearl-necklace.png",
+  },
+
+  {
+    id: 10,
+    name: "Elegant Pearl Necklace",
+    category: "Jewelry",
+    price: 120,
+    image: "/images/elegant-pearl-necklace.png",
+  },
+];
+
+export default products;
