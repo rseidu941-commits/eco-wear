827b57939657ddfa0d9e592663e81e641d20f645
Rukayatu <rseidu941@gmail.com>
2026-07-28 19:41:37 +0200

    update: product grid component completed

---
+++ src/Components/ProductGrid/ProductGrid.jsx
@@ -0,0 +1,24 @@
+// src/components/ProductGrid/ProductGrid.jsx
+import React from "react";
+import ProductCard from "../ProductCard/ProductCard";
+import products from "../../data/product"; 
+import "./ProductGrid.scss";
+
+function ProductGrid() {
+  return (
+    <section className="product-grid">
+      <div className="product-grid__container">
+        <h2 className="product-grid__title">Our Collection</h2>
+        
+        <div className="product-grid__list">
+          {products.map((item) => (
+           
+            <ProductCard key={item.id} product={item} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ProductGrid;
