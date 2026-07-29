fbe4aa019a5e39eee90b3d818c598b4e9e0003ae
Rukayatu <rseidu941@gmail.com>
2026-07-29 19:25:01 +0200

    home page created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import "./Home.scss";
+
+import Header from "../../components/Header/Header";
+import Hero from "../../components/Hero/Hero";
+import FeaturedProduct from "../../components/FeaturedProducts/FeaturedProducts";
+import WhyChoose from "../../components/WhyChoose/WhyChoose";
+import JoinOurCommunity from "../../components/JoinOurCommunity/JoinOurCommunity";
+import Footer from "../../components/Footer/Footer";
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <main className="home">
+      <Header/>
+      <Hero/>
+      <FeaturedProduct/>
+      <WhyChoose/>
+      <JoinOurCommunity/>
+      <Footer/>
+      </main>
   )
 }
 
-export default Home
+export default Home;
