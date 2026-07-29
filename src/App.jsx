453ab256b868069015a8724c9a05421a0cd8eb1e
Rukayatu <rseidu941@gmail.com>
2026-07-29 16:04:07 +0200

    productgrid deleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import "./App.scss";
 import Button from "./Components/Button/Button";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
-import ProductGrid from "./Components/ProductGrid/ProductGrid";
+
+import WhyChoose from "./Components/WhyChoose/WhyChoose";
+import JoinOurCommunity from "./Components/JoinOurCommunity/JoinOurCommunity";
+import Footer from "./Components/Footer/Footer";
 
 
 
@@ -17,7 +20,10 @@ function App() {
       <Hero />
        <Button />
       
-       <ProductGrid/>
+       
+       <WhyChoose/>
+       <JoinOurCommunity/>
+       <Footer/>
        
     </>
   );
