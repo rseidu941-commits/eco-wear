1c5ce176c47794505434e97b7a15eec3d6813178
Rukayatu <rseidu941@gmail.com>
2026-07-29 19:12:47 +0200

    featured product updated

--- src/Components/FeaturedProduct/FeaturedProduct.jsx
+++ src/Components/FeaturedProduct/FeaturedProduct.jsx
@@ -1,34 +1,35 @@
+import "./FeaturedProducts.scss";
 import products from "../../data/products";
 import ProductCard from "../ProductCard/ProductCard";
 import Button from "../Button/Button";
-import "./FeaturedProducts.scss";
 
 function FeaturedProducts() {
-
   const featuredProducts = products.slice(0, 4);
 
   return (
     <section className="featured-products">
+      <div className="featured-products__container">
 
-      <h2 className="featured-products__title">
-        Featured Products
-      </h2>
+        <h2 className="featured-products__title">
+          Featured Products
+        </h2>
 
-      <div className="featured-products__grid">
+        <div className="featured-products__grid">
+          {featuredProducts.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+            />
+          ))}
+        </div>
 
-        {featuredProducts.map((product) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-          />
-        ))}
+        <div className="featured-products__button">
+          <Button>
+            View All Products
+          </Button>
+        </div>
 
       </div>
-
-      <Button>
-        View All Products
-      </Button>
-
     </section>
   );
 }
